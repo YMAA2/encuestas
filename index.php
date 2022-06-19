3cef71d0d8942d7c1a83ee76f43f9a5a6b5f292f
YMAA2 <104746499+YMAA2@users.noreply.github.com>
2022-06-18 23:34:28 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    //var_dump($_GET);
+    /* echo ("url completada: ".$_POST["url"]);*/
+    if(isset($_GET["GENERAR"])){
+        //echo ("url completada: ".$_GET["url"]);
+    } 
+?>
+<html>
+    <form name="buscar" action="./index.php" method="GET">
+        <label for="url">URL K1000</label>
+        <input type="text" name="url" id="url">
+        <br>
+        <label for="url">URL K1000 COMPLETADA</label>
+        <input type="text" name="url_done" id="url_done" value="<?php if(isset($_GET['url'])) echo $_GET['url'];?>">
+        <br>
+        <input type="submit" value="GENERAR" name="GENERAR">
+    </form>
+</html>
+<?php
+?>
